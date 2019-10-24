3e8a54c895e30e05e5e7c68a25106ce56ceb88b3
Francesca Dreith <francesca.dreith@gmail.com>
2019-10-24 01:52:46 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+def using_push (colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift (bouroughs_in_nyc, new_neighborhood)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  new_neighborhood = "Staten Island"
+  bouroughs_in_nyc.unshift("Staten Island")
+end
+
+def using_pop (continents, deleted_string)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania","Antarctica" ]
+ continents.pop
+end
+
+def pop_with_args
+  
+end
+
+def using_unshift
+  
+end
