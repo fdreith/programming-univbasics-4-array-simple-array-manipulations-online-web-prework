d6eea8f2b15059fc448b77517e4c02e90ebaaac5
Francesca Dreith <francesca.dreith@gmail.com>
2019-10-24 02:02:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,10 +4,8 @@ def using_push (colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push(next_color)
 end
 
-def using_unshift (bouroughs_in_nyc, new_neighborhood)
-  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-  new_neighborhood = "Staten Island"
-  bouroughs_in_nyc.unshift("Staten Island")
+def using_unshift (array, item)
+  array.unshift(item)
 end
 
 def using_pop (continents, deleted_string)
